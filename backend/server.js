7e98b0705bc558cffcccc51891acb28e9ea308ac
hyunho4532 <kyounguk192@gmail.com>
2024-04-11 00:00:06 +0900

    2024-04-10 에러 작성 글 구현 중 (DynamoDB 연동 및 Insert 구현 완료)

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const https = require('https');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const aws = require("aws-sdk");
 const {errorTypeFromData} = require("./api/errorType/errorTypeFromData");
 const {platformFromData} = require("./api/platform/platformFromData");
 
+const docClient = new aws.DynamoDB.DocumentClient({ region: 'ap-northeast-2' });
 
 const corsOptions = {
     origin: '*',
@@ -12,6 +15,7 @@ const corsOptions = {
 
 const app = express();
 app.use(cors(corsOptions));
+app.use(bodyParser.json());
 
 app.post('/errorTypeData', (req, res) => {
     https.get('https://czwpwf5o4m.execute-api.ap-northeast-2.amazonaws.com/stage/error/write', (response) => {
@@ -31,6 +35,34 @@ app.post('/platformData', (req, res) => {
     }).on('error', (error) => {
         console.error(error);
     })
+})
+
+app.post('/errorBoardData', (req, res) => {
+
+    aws.config.update ({
+        region: 'ap-northeast-2',
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    })
+
+    const body = req.body;
+
+    console.log(body);
+
+    const params = {
+        TableName: 'errorBoard',
+        Item: body
+    };
+
+    docClient.put(params, (err, data) => {
+        if (err) {
+            console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
+            res.status(500).send('Error saving data to DynamoDB');
+        } else {
+            console.log('Added item:', JSON.stringify(data, null, 2));
+            res.json(body);
+        }
+    });
 })
 
 app.listen(50000, () => {
--- frontend/src/component/ErrorInsertButton.tsx
+++ frontend/src/component/ErrorInsertButton.tsx
@@ -2,15 +2,28 @@ import { useRecoilValue } from 'recoil'
 import '../view/write/scss/component/Button.scss'
 import { ErrorBoard } from '../model/ErrorBoard'
 import { errorBoard } from '../recoil/Atom'
+import uuid from 'react-uuid'
+import axios from 'axios';
 
 function ErrorInsertButton() {
 
     const errorBoardData = useRecoilValue<ErrorBoard>(errorBoard);
 
+    const data = {
+        id: uuid(),
+        selectedPlatformData: errorBoardData.selectedPlatformData,
+        errorTypeData: errorBoardData.errorTypeData,
+        errorFileData: errorBoardData.errorFileData
+    };
+
     const errorInsertButton = () => {
-        console.log(errorBoardData.selectedPlatformData);
-        console.log(errorBoardData.errorTypeData);
-        console.log(errorBoardData.errorFileData);
+        axios.post('http://localhost:50000/errorBoardData', data)
+            .then(response => {
+                console.log('응답 받음:', response.data);
+            })
+            .catch(error => {
+                console.error('에러 발생:', error);
+            });
     }
 
     return (
