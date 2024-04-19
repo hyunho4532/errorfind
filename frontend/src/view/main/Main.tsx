8d69fc2b50948cd3f654491567057ffbffa49037
hyunho4532 <kyounguk192@gmail.com>
2024-04-19 21:58:25 +0900

    2024-04-19 최신 에러 등록 조회 (페이징 처리 기능 구현 중)

--- frontend/src/view/main/Main.tsx
+++ frontend/src/view/main/Main.tsx
@@ -1,32 +1,44 @@
 import axios from "axios"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './scss/Main.scss'
-import { Card } from "@mui/material";
+import { Card, Pagination } from "@mui/material";
 
 function Main() {
 
     const [errorBoardData, setErrorBoardData] = useState([]);
+    const [page, setPage] = useState(1);
 
-    axios.get("http://localhost:50000/errorBoardData/get")
+    const handlePageChange = (page: any) => {
+        setPage(page);
+    }
+
+    useEffect(() => {
+        axios.get("http://localhost:50000/errorBoardData/get")
         .then((response) => {
-            console.log(response.data); 
             setErrorBoardData(response.data);
         })
         .catch((error) => {
             console.error(error);
         })
+    })
 
     return (
-        <div className="main-component">
+        <>
+            <div className="main-component">
                 {errorBoardData.map((error: any, index) => (
-                    <Card className="main-card-component">
-                        <div key={index}>
-                            <p>{error.errorTypeData}</p>
-                            <p className="main-card-formatted">{error.formattedDateData}</p>
-                        </div>
-                    </Card>
+                    <div key={index} className="main-card-component">
+                        <p>{error.errorTypeData}</p>
+                        <p className="main-card-formatted">{error.formattedDateData}</p>
+                    </div>
                 ))}
-        </div>
+            </div>
+
+            <Pagination
+                count={10}
+                onChange={handlePageChange}
+                page={page}
+            />
+        </>
     )
 }
 
