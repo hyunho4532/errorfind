f56f5910be9f80c10a3790709d024e5c2f4c25be
hyunho4532 <kyounguk192@gmail.com>
2024-04-20 20:43:41 +0900

    2024-04-20 메인 페이지 구현 중 (헤더 구현 완료 (로고 아이콘))

--- frontend/src/component/header/Header.tsx
+++ frontend/src/component/header/Header.tsx
@@ -3,8 +3,11 @@ import './Header.scss'
 function Header() {
     return (
         <div className="header-main">
-            <p className="header-login">로그인</p>
-            <p className="header-error-write">에러 등록하기</p>
+            <img width={140} height={60} className="header-logo" src="../../../public/errorFindLogo.jpg" />
+            <div className="header-main-title">
+                <p className="header-login">로그인</p>
+                <p className="header-error-write">에러 등록하기</p>
+            </div>
         </div>
     )
 }
