8f0fa55f9c56e57f9a53accf9b93a91644cf1cad
hyunho4532 <kyounguk192@gmail.com>
2024-04-20 22:56:04 +0900

    2024-04-20 로그인 모달 창 구현 중 (구글 로그인 구현 중, 모달 창 구현 완료)

--- frontend/src/component/header/Header.tsx
+++ frontend/src/component/header/Header.tsx
@@ -17,7 +17,7 @@ function Header() {
     return (
         <>
             <div className="header-main">
-                <img width={140} height={60} className="header-logo" src="../../../public/errorFindLogo.jpg" />
+                <img width={140} height={60} className="header-logo" src="../../../public/errorfind_logo.jpg" />
                 <div className="header-main-title">
                     <button className="header-login" onClick={loginClick}>로그인</button>
                     <p className="header-error-write">에러 등록하기</p>
@@ -29,9 +29,28 @@ function Header() {
                 onClose={modalClose}>
 
                 <div className="modal">
-                    <Typography className='modal-error-insert'>ErrorFind를 이용해주셔서 감사합니다.</Typography>
-                </div>
+                    <Typography className="modal-login-title">ErrorFind를 이용해주셔서 감사합니다.</Typography>
+                    <Typography className="modal-login-errorfind-platform">erorrFind는 다른 사람들에게 에러를 공유해 서로 공유하면서 해결하는 플랫폼을 지원하겠습니다!</Typography>
+                    <Typography className="modal-login-options-prompt">아래 카카오, 구글 로그인 중 원하는 로그인을 선택해주세요!!</Typography>
 
+                    <div className="modal-login-kind">
+                        <div>
+                            <button>
+                                <img width={60} height={60} src="../../../public/google_logo.png" />
+                            </button>
+                            
+                            <p className="modal-login-google-text">구글 로그인</p>
+                        </div>
+
+                        <div>
+                            <button>
+                                <img width={60} height={60} src="../../../public/kakao.jpg" />
+                            </button>
+
+                            <p className="modal-login-kakao-text">카카오 로그인</p>
+                        </div>
+                    </div>
+                </div>
             </Modal>
         </>
     )
