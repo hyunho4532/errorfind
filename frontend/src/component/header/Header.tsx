cab28d2fd329b7bc85fdc2e631a74d94b305ac46
hyunho4532 <kyounguk192@gmail.com>
2024-04-20 19:21:06 +0900

    2024-04-20 메인 페이지 구현 중 (헤더 구현 완료)

---
+++ frontend/src/component/header/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.scss'
+
+function Header() {
+    return (
+        <div className="header-main">
+            <p className="header-login">로그인</p>
+            <p className="header-error-write">에러 등록하기</p>
+        </div>
+    )
+}
+
+export default Header
