34d33f72b084919fe55f3d6fbf6279ee4af5b5ae
hyunho4532 <kyounguk192@gmail.com>
2024-04-20 22:25:32 +0900

    2024-04-20 로그인 모달 창 구현 중 (카카오 로그인, 구글 로그인)

--- frontend/src/component/header/Header.tsx
+++ frontend/src/component/header/Header.tsx
@@ -1,14 +1,39 @@
+import { useState } from 'react'
 import './Header.scss'
+import { Modal, Typography } from '@mui/material';
 
 function Header() {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const loginClick = () => {
+        setModalIsOpen(true);
+    }
+
+    const modalClose = () => {
+        setModalIsOpen(false);
+    }
+
     return (
-        <div className="header-main">
-            <img width={140} height={60} className="header-logo" src="../../../public/errorFindLogo.jpg" />
-            <div className="header-main-title">
-                <p className="header-login">로그인</p>
-                <p className="header-error-write">에러 등록하기</p>
+        <>
+            <div className="header-main">
+                <img width={140} height={60} className="header-logo" src="../../../public/errorFindLogo.jpg" />
+                <div className="header-main-title">
+                    <button className="header-login" onClick={loginClick}>로그인</button>
+                    <p className="header-error-write">에러 등록하기</p>
+                </div>
             </div>
-        </div>
+
+            <Modal
+                open={modalIsOpen}
+                onClose={modalClose}>
+
+                <div className="modal">
+                    <Typography className='modal-error-insert'>ErrorFind를 이용해주셔서 감사합니다.</Typography>
+                </div>
+
+            </Modal>
+        </>
     )
 }
 
